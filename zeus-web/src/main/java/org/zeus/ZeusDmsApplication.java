a8452743540fe02a6c7f05315da7907aa2009bae
aichaellee <793708025@qq.com>
2019-01-08 17:40:06 +0800

    test redis config

--- zeus-web/src/main/java/org/zeus/ZeusDmsApplication.java
+++ zeus-web/src/main/java/org/zeus/ZeusDmsApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
@@ -22,6 +24,10 @@ public class ZeusDmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZeusDmsApplication.class, args);
 	}
+    @Bean
+    public ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
 
 }
 
